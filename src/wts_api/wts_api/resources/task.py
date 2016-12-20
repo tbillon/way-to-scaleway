066e0a800aa1a38be3939453e3d069d31970df49
Thibault Billon <thibault.billon@gmail.com>
2016-12-20 09:52:07 +0100

    Add a video fetch url

--- src/wts_api/wts_api/resources/task.py
+++ src/wts_api/wts_api/resources/task.py
@@ -1,9 +1,9 @@
-from flask import request
+from flask import request, send_from_directory
 from flask_restful import fields, Resource
 
 from wts_api.common import SessionScope
 from wts_api.common.utils import my_marshal, valid_task, valid_uuid
-from wts_db import models
+from wts_db import models, config
 from wts_worker import worker
 
 
@@ -78,3 +78,17 @@ class TaskList(Resource):
         with SessionScope() as session:
             tasks = session.query(models.Task).all()
             return my_marshal(tasks, task_fields, envelope='tasks')
+
+
+class Video(Resource):
+    def get(self, uuid=None):
+        if uuid is None:
+            return {'error': 'No UUID provided'}, 400
+        if not valid_uuid(uuid):
+            return {'error': "'{}' is not a valid UUID".format(uuid)}, 400
+
+        with SessionScope() as session:
+            t = session.query(models.Task).filter(models.Task.uuid == uuid).first()
+            if t is None:
+                return {'error': "'{}' unknown video".format(uuid)}, 404
+        return send_from_directory(config.INCOMING_VIDEO_URI, t.dst_url)
