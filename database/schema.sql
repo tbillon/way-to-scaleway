ecec217f9ddfeb2f078e5c02b911f772f0a7dd26
Thibault Billon <thibault.billon@gmail.com>
2016-12-20 09:31:30 +0100

    Automatic update of task.upd_date

--- database/schema.sql
+++ database/schema.sql
@@ -9,3 +9,16 @@ CREATE TABLE task (
        dst_url VARCHAR(2083) DEFAULT NULL,
        src_url VARCHAR(2083) NOT NULL
 );
+
+
+CREATE OR REPLACE FUNCTION task_update_upd_date()
+RETURNS trigger AS $$
+BEGIN
+	NEW.upd_date = CURRENT_TIMESTAMP;
+	RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+
+CREATE TRIGGER trigger_task_update_upd_date BEFORE UPDATE ON task FOR EACH ROW
+       WHEN (NEW.status <> OLD.status) EXECUTE PROCEDURE  task_update_upd_date();
