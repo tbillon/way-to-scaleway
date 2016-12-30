4480003e9c502151e08c083f66fb6b8ae5c82576
Thibault Billon <thibault.billon@gmail.com>
2016-12-30 15:36:36 +0100

    Build broker URL from credentials

--- wts_worker/wts_worker/app.py
+++ wts_worker/wts_worker/app.py
@@ -13,25 +13,30 @@ import sys
 from celery import Celery
 
 import wts_worker
-from wts_worker.settings import Settings
+from wts_worker.settings import Settings, broker_url
 
 
 app = Celery('wts_worker',
-             broker='amqp://localhost',
+             broker='amqp://',
              backend='rpc://',
              include=['wts_worker.worker'])
+
+
+credentials = os.environ.get('CREDENTIALS')
+if credentials is None:
+    print 'Could not load credentials file'
+    sys.exit(1)
+Settings.load_from_file(credentials)
+
+app.conf.update(
+    broker_url=broker_url(Settings),
+)
+wts_worker.DatabaseTask.init_database()
 
 
 def main():
     """Program entry point
     """
-    credentials = os.environ.get('CREDENTIALS')
-    if credentials is None:
-        print 'Could not load credentials file'
-        sys.exit(1)
-    Settings.load_from_file(credentials)
-
-    wts_worker.DatabaseTask.init_database()
     app.start()
 
 
--- wts_worker/wts_worker/settings.py
+++ wts_worker/wts_worker/settings.py
@@ -11,3 +11,14 @@ DEFAULTS = {'DEBUG': False, 'OUTPUT_DIRECTORY': '/videos/incoming'}
 
 
 Settings = config.ConfigLoader(defaults=DEFAULTS)
+
+
+def broker_url(settings):
+    """Return the broker url based on environment variables"""
+    broker = 'amqp://'
+    broker += settings.get('BROKER_USER') or 'guest'
+    broker += ':' + (settings.get('BROKER_PASSWORD') or 'guest')
+    broker += '@' + (settings.get('BROKER_HOST') or 'localhost')
+    broker += ':' + (settings.get('BROKER_PORT') or '5672')
+
+    return broker
